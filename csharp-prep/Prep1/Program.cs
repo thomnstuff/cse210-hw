82da2eb28dd8923f1baf581b13655df8d77cda29
thomnstuff <thomness03@gmail.com>
2023-01-07 18:38:52 -0800

    prep works

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string name = Console.ReadLine();
+        
+        Console.Write("What is your last name? ");
+        string lname = Console.ReadLine();
+        
+        Console.WriteLine();
+
+        Console.WriteLine($"Your name is {lname}, {name} {lname}.");
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+        int x = int.Parse(grade);
+
+        if (x >= 90) {
+            Console.WriteLine("Your Grade is: A ");
+        }
+        else if (x >= 80) {
+            Console.WriteLine("Your Grade is: B ");
+        }
+        else if (x >= 70) {
+            Console.WriteLine("Your Grade is: C ");
+        }
+        else if (x >= 60) {
+            Console.WriteLine("Your Grade is: D ");
+        }
+        else {
+            Console.WriteLine("Your Grade is: F");
+        }
     }
 }
